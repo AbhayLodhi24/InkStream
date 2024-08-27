7e5bfd4ffd0288b46cb7a965473d99263ee83f34
Abhay Kumar Lodhi <95959501+AbhayLodhi24@users.noreply.github.com>
2024-08-27 23:32:17 +0530

    Update DB url in app.js

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/blogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const postSchema = new mongoose.Schema({
   title: String,
